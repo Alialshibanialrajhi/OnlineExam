60243459b5a578016c23a0b59542e1b840396efc
Alialshibanialrajhi <46638803+Alialshibanialrajhi@users.noreply.github.com>
2019-02-11 21:28:41 +0200

    onlinexam - ali files
    onlineExam,

---
+++ dbConfig.php
@@ -0,0 +1,15 @@
+<?php
+   
+        $database_connection = mysqli_connect("localhost", "root","", "onlineexam");
+       
+       if($database_connection == false){
+           die("ERROR" . mysqli_connect_error());
+       }
+      
+                                
+           
+
+
+
+
+
